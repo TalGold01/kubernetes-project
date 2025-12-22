82aa3885683348795595e93de517917f49e00103
Talold01 <TalGold201@gmail.com>
2025-12-22 20:48:10 +0200

    Updated The EKS.tf and misc

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,15 +12,12 @@ module "vpc" {
   public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
   private_subnets = ["10.10.101.0/24", "10.10.102.0/24"]
 
-  # FinOps: No NAT Gateway.
-  # Nodes MUST be in public subnets to reach the internet (ECR/AWS APIs).
+  # FinOps: No NAT Gateway
   enable_nat_gateway = false 
   
-  # FIX: Mandatory for public nodes without NAT Gateway
-  # This fixes the "Ec2SubnetInvalidConfiguration" error you saw
+  # Mandatory for public nodes
   map_public_ip_on_launch = true
 
-  # CRITICAL FOR ALB CONTROLLER:
   public_subnet_tags = {
     "kubernetes.io/role/elb" = "1"
   }
@@ -43,37 +40,46 @@ module "eks" {
   version = "~> 19.21"
 
   cluster_name    = "luxe-eks"
-  
-  # FinOps Upgrade: Use 1.32 to stay in Standard Support ($0.10/hr)
-  # Versions 1.28, 1.29, 1.30, and 1.31 are all in Extended Support ($0.60/hr)
   cluster_version = "1.32"
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
 
-  # Public access is REQUIRED because our nodes (in public subnets) 
-  # will talk to the control plane via the public internet endpoint.
   cluster_endpoint_public_access = true
+
+  # Auth ConfigMap (Crucial for CodeBuild)
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::349020400385:role/luxe-codebuild-role"
+      username = "codebuild"
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = "arn:aws:iam::349020400385:user/project-admin"
+      username = "admin"
+      groups   = ["system:masters"]
+    }
+  ]
 
   ##########################
   # EKS Managed Node Groups
   ##########################
   eks_managed_node_groups = {
     spot_nodes = {
-      # CRITICAL Fix for FinOps (No NAT GW):
-      # We force nodes into Public Subnets so they can pull images from ECR.
       subnet_ids = module.vpc.public_subnets
-      
-      # Assign public IPs so they can route out to the internet
       associate_public_ip_address = true
 
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      # DEMO SETTINGS:
+      # We use 3 micros to ensure enough RAM for system pods + your app.
+      min_size     = 2
+      max_size     = 4
+      desired_size = 3 
 
       capacity_type  = "SPOT"
-      instance_types = ["t3.small"] 
+      instance_types = ["t3.micro"] # Free tier eligible type
       
       labels = {
         role = "spot"
